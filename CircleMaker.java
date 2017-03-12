7449ca5fc31606f22bc8f80cd85641312cd66e52
Caroline Danzi <danzicr@miamioh.edu>
2017-03-11 23:40:34 -0500

    Added documentation and fixed indentation

--- CircleMaker.java
+++ CircleMaker.java
@@ -1,3 +1,12 @@
+/**
+* This class creates an ArrayList of Circle objects that represent
+* a collection of circles which fill a larger circle. The location and radii
+* of the circles are chosen randomly, though no two circles touch. 
+* 
+* @author Bo Brinkman
+* @version 2017-03-11
+*/
+
 import java.util.ArrayList;
 
 public class CircleMaker {
@@ -12,33 +21,51 @@ public class CircleMaker {
     static double area = 0.0;
     static final double targetRatio = 0.7;
 
+	/**
+	* Create an ArrayList of Circle objects that randomly fill a larger circle. No two
+	* circles touch, and the total area of the circles combined divided by 
+	* the total area to fill is less than the target ratio.
+	* 
+	* @return an ArrayList of Circle objects
+	*/
     public static ArrayList<Circle> makeCircles() {
-	n = 0;
-	area = 0.0;
+		n = 0;
+		area = 0.0;
 
-	while (area/fillArea < targetRatio && n < maxN) {
-	    x[n] = -1.0 + 2.0*Math.random();
-	    y[n] = -1.0 + 2.0*Math.random();
-	    r[n] = minR + (maxR-minR)*Math.random();
+		// Continue generating circles until we either draw the max number
+		// of circles or until the area of how much we have filled is greater
+		// than our target ratio
+		while (area/fillArea < targetRatio && n < maxN) {
+			// Select a random point inside the window and 
+			// a random radius size that is within the range [minR, maxR) 
+			x[n] = -1.0 + 2.0*Math.random();
+			y[n] = -1.0 + 2.0*Math.random();
+			r[n] = minR + (maxR-minR)*Math.random();
 
-	    for (int i=0; i < n; i++) {
-		double d = Math.sqrt((x[n]-x[i])*(x[n]-x[i]) +
-			(y[n]-y[i])*(y[n]-y[i]));
-		if (d < r[n]+r[i]) r[n] = d - r[i];
-	    }
+			// Go through each existing circle - if the new circle overlaps
+			// any of them, decrease its radius by the distance they overlap
+			for (int i=0; i < n; i++) {
+				double d = Math.sqrt((x[n]-x[i])*(x[n]-x[i]) +
+					(y[n]-y[i])*(y[n]-y[i]));
+				if (d < r[n]+r[i]) r[n] = d - r[i];
+			}
 
-	    double d = Math.sqrt(x[n]*x[n] + y[n]*y[n]) + Math.abs(r[n]);
-	    if (r[n] >= minR &&
-		    d <= 1.0) {
-		area += Math.PI*r[n]*r[n];
-		n++;
-	    }
+			// If the radius is greater than the minimum radius and
+			// the circle is actually inside the larger circle we are filling,
+			// move on to the next circle. Otherwise, try again. 
+			double d = Math.sqrt(x[n]*x[n] + y[n]*y[n]) + Math.abs(r[n]);
+			if (r[n] >= minR && d <= 1.0) {
+				area += Math.PI*r[n]*r[n];
+				n++;
+			}
+		}
+
+		// Build Circle objects using the xy-coordinate data and radii 
+		// and put each Circle object in the ArrayList
+		ArrayList<Circle> ret = new ArrayList<Circle>();
+		for (int i=0; i < n; i++) {
+			ret.add(new Circle(x[i], y[i], r[i]));
+		}
+		return ret;
 	}
-
-	ArrayList<Circle> ret = new ArrayList<Circle>();
-	for (int i=0; i < n; i++) {
-	    ret.add(new Circle(x[i], y[i], r[i]));
-	}
-	return ret;
-    }
 }
--- CircleMakerSketch.java
+++ CircleMakerSketch.java
@@ -1,37 +1,72 @@
+/**
+* Creates a Processing sketch that contains a large circle filled with 
+* smaller circles. The large circle is divided evenly into three rings,
+* where the smaller circles that fill the middle ring are colored darker
+* than those that fill the outer and innermost rings. 
+* 
+* @author Bo Brinkman
+* @version 2017-03-11
+*/
+
 import processing.core.PApplet;
 import java.util.ArrayList;
 
 public class CircleMakerSketch extends PApplet {
+	
+	/**
+	* Starts the sketch
+	*/
     public static void main(String[] args){
-	PApplet.main("CircleMakerSketch");
+		PApplet.main("CircleMakerSketch");
     }
 
+	/**
+	* Sets the size of the window
+	*/
     public void settings() {
     	size(512, 512);
     }
 
+	/**
+	* Required method for Processing. Will draw 60 times per second
+	*/
     public void draw()  {
     }
 
+	/**
+	* Sets up the sketch by drawing 
+	*/
     public void setup() {
-	background(0);
+		// Set background color to black
+		background(0);
 
-	ArrayList<Circle> circles = CircleMaker.makeCircles();
+		// Create an ArrayList of circle objects to draw
+		ArrayList<Circle> circles = CircleMaker.makeCircles();
 
-	scale(width/2);
-	translate(1, 1);
-	noStroke();
-	ellipseMode(CENTER);
-	for (int i=0; i < circles.size(); i++) {
-	    double d = Math.sqrt(circles.get(i).x*circles.get(i).x
-	    			+circles.get(i).y*circles.get(i).y);
-	    if (d > 2/3.0 || d < 1/3.0) {
-		fill(255 - random(63));
-	    } else {
-		fill(127 - random(63));
-	    }
-	    ellipse((float)circles.get(i).x, (float)circles.get(i).y, 
-	          2*(float)circles.get(i).r, 2*(float)circles.get(i).r);
-	}
+		scale(width/2);
+		// Puts the origin at the center of the window rather than
+		// the upper-left corner
+		translate(1, 1);
+		// No line
+		noStroke();
+		ellipseMode(CENTER);
+		
+		// For each circle in the ArrayList, 
+		for (int i=0; i < circles.size(); i++) {
+			// Calculate the distance between the center
+			// of the circle and the origin. The main large circle will
+			// be split into three rings - the two outer rings will be
+			// filled with circles that are lighter gray than the middle ring
+			double d = Math.sqrt(circles.get(i).x*circles.get(i).x
+						+circles.get(i).y*circles.get(i).y);
+			if (d > 2/3.0 || d < 1/3.0) {
+				fill(255 - random(63));
+			} else {
+				fill(127 - random(63));
+			}
+			// Draw the circle 
+			ellipse((float)circles.get(i).x, (float)circles.get(i).y, 
+				  2*(float)circles.get(i).r, 2*(float)circles.get(i).r);
+		}
     }
 }
