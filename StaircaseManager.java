ab35027d4e3a2f22463b26a187ef69e72041762e
Caroline Danzi <danzicr@miamioh.edu>
2017-05-11 18:32:15 -0400

    Added stopping condition and threshold logic to StaircaseManager and updated tester

--- StaircaseManager.java
+++ StaircaseManager.java
@@ -4,9 +4,10 @@
 * color discrimination tests). When a test subject responds to a stimulus,
 * the stimulus should decrease in intensity. If they do not respond to a stimulus,
 * the stimulus should increase in intensity so they have a better chance of 
-* sensing it. 
+* sensing it. The trials stop when the test subject has not responded to a stimulus
+* in at least X number of trials (defined in parameters). 
 * 
-* The threshold is the intensity at which 50% of the responses are 'yes'. According to
+* The threshold is the intensity at which >=50% of the responses are 'yes'. According to
 * the American Speech and Hearing Association, "[y]ou have found threshold when you find 
 * the lowest level at which the person responds in at least 50% of three trials”  
 * 
@@ -14,15 +15,17 @@
 * @version 2017-05-07 
 */
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
 
 public class StaircaseManager {
 	
 	private double curVal;
 	private double stepSize;
 	private double correctDirection;
+	private final int NUM_NOS_TO_STOP = 3;
 	private boolean thresholdReached;
-	private double threshold;
-	HashMap<Double, Integer> intensityTrials;
+	HashMap<Double, Trials> intensityTrials;
 	
 	/**
 	* Constructor - requires a starting value, a step size, and an indication of whether 
@@ -36,15 +39,14 @@ public class StaircaseManager {
 	* 
 	* @param startValue The value at which to start the staircase 
 	* @param stepSize The size of each step - when a user responds to a stimulus, what is the amount we should increase or decrease?
-	* @param correctDirection true if a correct response indicates the stimulus should be increased, false otherwise 
+	* @param correctDirection true if a correct response indicates the stimulus should increase numerically, false otherwise 
 	*/
 	public StaircaseManager(double startValue, double stepSize, boolean shouldIncrease) {
 		this.curVal = startValue;
 		this.stepSize = stepSize;
 		this.correctDirection = shouldIncrease ? 1 : -1;
 		this.thresholdReached = false;
-		this.threshold = -1;
-		intensityTrials = new HashMap<Double, Integer>();
+		intensityTrials = new HashMap<Double, Trials>();
 	}
 	
 	/**
@@ -55,11 +57,35 @@ public class StaircaseManager {
 	* @return The value of the next stimulus 
 	*/
 	public double getNextVal(boolean isCorrect) {
-		// TODO: implement logic for testing for the stop condition and 
-		// update thresholdReached as needed 
+		Trials t = intensityTrials.get(curVal);
 		if(isCorrect) {
+			if(t == null) {
+				// New intensity value 
+				intensityTrials.put(curVal, new Trials(1, 1));
+			} else {
+				// Update the number of trials 
+				t = new Trials(t.yes + 1, t.total + 1);
+				intensityTrials.put(curVal, t);
+			}
+			// Add a step in the correct direction to get the next value 
 			curVal += correctDirection * stepSize;
 		} else {
+			// Each time you update the 'no's', check to see if you have reached
+			// three - if you have, you have reached the stop condition 
+			if(t == null) {
+				// New intensity value 
+				intensityTrials.put(curVal, new Trials(0, 1));
+			} else {
+				// Update the number of trials
+				t = new Trials(t.yes, t.total + 1);
+				intensityTrials.put(curVal, t);
+				// If the number of no's is equal to or greater than the number of no's 
+				// required for our stop condition, then we have reached the threshold 
+				if(t.total - t.yes >= NUM_NOS_TO_STOP) {
+					thresholdReached = true;
+				}
+			}
+			// Subtract a step in the correct direction to get the next value 
 			curVal -= correctDirection * stepSize;
 		}
 		return curVal;
@@ -68,7 +94,9 @@ public class StaircaseManager {
 	/**
 	* Returns true if we have reached the threshold according to the 
 	* termination condition; false otherwise. False indicates that 
-	* more trials are needed in order to determine the threshold. 
+	* more trials are needed in order to determine the threshold. The threshold
+	* is reached when a test subject has not responded to the stimulus at a single
+	* intensity in a specified number of trials. 
 	* 
 	* @return true if the threshold can be determined from the given data; false otherwise 
 	*/
@@ -79,14 +107,33 @@ public class StaircaseManager {
 	/**
 	* Gets the threshold if one has been reached. If the termination condition for
 	* this staircase procedure has not yet been reached, an exception will be thrown
-	* since a threshold is unable to be determined from the given data. 
+	* since a threshold is unable to be determined from the given data. The threshold
+	* is the least intense stimulus at which the test subject responded in at least 50% of the trials. 
 	* 
 	* @return the threshold, if one has been reached 
 	*/
 	public double getThreshold() throws Exception {
 		if(thresholdReached) {
-			// TODO: add logic for determining the threshold, which is the intensity level
-			// at which >= 50% of the responses are 'yes' 
+			// Help from http://stackoverflow.com/questions/1066589/iterate-through-a-hashmap
+			// Based on the direction of increasing intensity, an increase in intensity may in some cases
+			// mean increasing values and in some cases decreasing values 
+			double threshold = correctDirection < 0 ? Double.MAX_VALUE : Double.MIN_VALUE;
+			for (Map.Entry<Double, Trials> entry : intensityTrials.entrySet()) {
+				double intensity = entry.getKey();
+				System.out.println(intensity+ " " + entry.getValue());
+				// If the current intensity is less intense than the current threshold, see if the response 
+				// rate is greater than or equal to 50% - if it is, then it is the new threshold 
+				// Note that if the direction of increasing intensity is 1, this means that the threshold will be 
+				// the LARGEST intensity, whereas if the direction of increase is -1, the threshold will be
+				// the SMALLEST intensity 
+				if((correctDirection == -1 && intensity < threshold) || (correctDirection == 1 && intensity > threshold)) {
+					Trials t = entry.getValue();
+					double percentCorrect = (double)t.yes / t.total;
+					if(percentCorrect >= .5) {
+						threshold = intensity;
+					}
+				}
+			}
 			return threshold;
 		} else {
 			throw new Exception("Tried to get threshold before threshold was reached");
--- StaircaseManagerTester.java
+++ StaircaseManagerTester.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
 public class StaircaseManagerTester {
-	public static void main(String[] args) {
-		StaircaseManager s = new StaircaseManager(5, 1, false);
-		System.out.println("Expected 4, actual: " + s.getNextVal(true));
-		System.out.println("Expected 5, actual: " + s.getNextVal(false));
-		StaircaseManager m = new StaircaseManager(5.5, 0.5, true);
-		System.out.println("Expected 5, actual: " + m.getNextVal(false));
-		System.out.println("Expected 5.5, actual: " + m.getNextVal(true));
+	public static void main(String[] args) throws Exception{
+		Scanner in = new Scanner(System.in);
+		System.out.print("Starting value: ");
+		double start = in.nextDouble();
+		System.out.print("Step size: ");
+		double step = in.nextDouble();
+		System.out.print("Enter '1' if when a test subject responds correctly, the intensity should increase numerically ");
+		System.out.print("ex: if the current intensity is 5 and the step size is 1 and a test subject is able to detect it, "); 
+		System.out.print("enter true if the next intensity should be 6) or '0' if it should decrease numerically");
+		int direction = in.nextInt();
+		boolean increase = direction == 1 ? true : false;
+		StaircaseManager s = new StaircaseManager(start, step, increase);
+		String test;
+		
+		while(!s.thresholdReached()) {
+			test = in.next();
+			if(test.equalsIgnoreCase("t")) {
+				System.out.println("Next value: " + s.getNextVal(true));
+			} else if(test.equalsIgnoreCase("f")) {
+				System.out.println("Next value: " + s.getNextVal(false));
+			} else if(test.equalsIgnoreCase("q")) {
+				System.exit(0);
+			} else if(test.equalsIgnoreCase("break")) {
+				break;
+			}
+		}
+		System.out.println("Threshold: " + s.getThreshold());
 	}
 }
